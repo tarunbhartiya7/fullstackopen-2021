6d494d656b955af1a534547aaed012d69b948834
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2021-09-27 21:38:57 +0530

    set default value for unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -28,9 +28,9 @@ const Statistics = ({ good, neutral, bad }) => {
 }
 
 const App = () => {
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   const handleGoodClick = () => setGood(good + 1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
