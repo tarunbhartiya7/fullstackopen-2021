be1e7d554aad4651e5b2ef1978a6bb81b35c6a10
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2021-09-27 13:38:47 +0530

    part1 anecdote solution

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState([0, 0, 0, 0, 0, 0, 0])
+  const maxPoints = points.indexOf(Math.max(...points))
+
+  const nextAnecdote = () =>
+    setSelected(randomIntFromInterval(0, anecdotes.length - 1))
+
+  const addVote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected] || 0} votes</div>
+      <button onClick={addVote}>vote</button>
+      <button onClick={nextAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[maxPoints]}</div>
+    </>
+  )
+}
+
+export default App
